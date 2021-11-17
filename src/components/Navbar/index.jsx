c5328eb7fc8861714e6ef86c3af932296291b5e0
k-737 <kamronalimov80@gmail.com>
2021-11-17 09:42:33 +0500

    navbar created

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,40 +3,52 @@ import React from "react";
 import "./style.css";
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <nav className="navbar navbar-expand-lg navbar-light Main">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          logo
+        </a>
+        <button
+          className="navbar-toggler ml-auto"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">current</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">
-              Disabled
-            </a>
-          </li>
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Education
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                contact me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
